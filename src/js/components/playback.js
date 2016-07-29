7f6c9388d2a6aa9a4930278d74b6d730c2950590
Patrick El-Hage <patrick.elhage@gmail.com>
2016-07-28 20:02:00 -0700

    renamed jsx to js

---
+++ src/js/components/playback.js
@@ -0,0 +1,35 @@
+var React = require('react');
+
+module.exports = React.createClass({
+
+	validateID: function() {
+		return true;
+	},
+
+	getInitialState: function() {
+		return { style: {
+			width: window.innerWidth + 'px',
+			height: window.innerHeight + 'px'
+			}
+		}
+	},
+
+  componentDidMount: function() {
+    window.addEventListener('resize', this.handleResize);
+  },
+
+  handleResize: function() {
+    this.setState({style: {
+  		width: window.innerWidth + 'px',
+			height: window.innerHeight + 'px'
+			}
+		});
+  },
+
+	render: function() {
+		return (<iframe style={this.state.style}
+			src={"http://www.youtube.com/embed/"+ this.props.params.videoid + "?autoplay=1"}
+			frameBorder="0"
+			allowFullScreen></iframe>);
+	}
+});
---
+++ src/js/components/video-row.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import Slider from 'react-slick';
+
+// import Thumbnail from './upload/Thumbnail';
+
+class VideoRow extends Component {
+
+  render() {
+    var settings = {
+      arrows: true,
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 4
+        }
+      }]
+    }
+
+    const { videos, category, onVideoClick } = this.props
+
+    let list = videos.map(function(video, index) {
+      return (<div key={video.videoIndex} onClick={onVideoClick}>
+        <div className="tile">
+          <div className="tile__media">
+            <div>
+              <img className="tile__img" src={"http://img.youtube.com/vi/"+video.id+"/0.jpg"} />
+            </div>
+          </div>
+          <div className="tile__details">
+            <div className="tile__title">
+              {video.description}
+            </div>
+          </div>
+        </div>
+      </div>)
+    });
+
+    return (
+      <div className="row">
+        <span className="video-category">{category}</span>
+          <Slider {...settings}>
+            {list}
+          </Slider>
+      </div>
+    );
+  }
+}
+
+export default VideoRow
