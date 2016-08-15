6f74a75c7237cf3f2bcfed2f99fd4a3cde186dc0
Patrick El-Hage <patrick.elhage@gmail.com>
2016-08-15 01:54:54 -0700

    Added more comments to action types file

--- src/js/actions/types.js
+++ src/js/actions/types.js
@@ -1,14 +1,17 @@
+// Authentication related acitons
 export const AUTH_USER = 'auth_user';
 export const DEAUTH_USER = 'deauth_user';
 export const AUTH_ERROR = 'auth_error';
+// Library related actions
 export const FETCH_LIBS = 'fetch_libs'
 export const ADD_LIB = 'add_lib'
 export const LIB_NAME = 'lib_name'
 export const ADD_CATEGORY = 'add_category'
-export const CURR_VID = 'curr_vid'
-export const ADD_VID = 'add_vid'
-export const REPLACE_CURRENT_VIDEO = 'replace_current_video'
 export const REPLACE_CURRENT_LIBRARY = 'replace_current_library'
 export const FETCH_LIB_BY_ID = 'fetch_lib_by_id'
-export const REMOVE_VIDEO = 'remove_video'
 export const REMOVE_LIB = 'remove_lib'
+export const ADD_VID = 'add_vid'
+export const REMOVE_VIDEO = 'remove_video'
+// Video related actions
+export const CURR_VID = 'curr_vid'
+export const REPLACE_CURRENT_VIDEO = 'replace_current_video'
