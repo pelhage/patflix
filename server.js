ef1261175d84adad913fa263f1c875d311810916
Patrick El-Hage <patrick.elhage@gmail.com>
2016-08-18 15:38:52 -0700

    Test removing webpack middleware in production

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-/* do webpack stuff */
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
-var compiler = webpack(config)
-
-app.use(webpackDevMiddleware(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-  historyApiFallback: true
-}))
-
-app.use(webpackHotMiddleware(compiler))
-/* end do webpack stuff */
+// /* do webpack stuff */
+// var webpack = require('webpack')
+// var webpackDevMiddleware = require('webpack-dev-middleware')
+// var webpackHotMiddleware = require('webpack-hot-middleware')
+// var config = require('./webpack.config')
+// var compiler = webpack(config)
+//
+// app.use(webpackDevMiddleware(compiler, {
+//   noInfo: true,
+//   publicPath: config.output.publicPath,
+//   historyApiFallback: true
+// }))
+//
+// app.use(webpackHotMiddleware(compiler))
+// /* end do webpack stuff */
 
 
 // for parsing application/json
