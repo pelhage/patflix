1d95d783bcba7a8e11a7d5fa287b62c9d9181171
Patrick El-Hage <patrick.elhage@gmail.com>
2016-08-21 19:33:48 -0700

    Improvements to Libraries route and to typography and flexbox layouts

--- src/js/components/libraries.js
+++ src/js/components/libraries.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import * as actions from '../actions'
 import { Link } from 'react-router';
 
+import { FormButton } from './form'
+
 class Libraries extends Component {
   constructor(props) {
     super(props)
@@ -27,11 +29,33 @@ class Libraries extends Component {
     }
     return []
   }
-
+//
+  // H1
+  // Div.flex-direction
   render() {
     const { libraries } = this.props
     if (!libraries) {
-      return (<div>You don't have any libraries</div>);
+      return (<div className="flex justify-center">
+        <div className="flex--lg flex-col">
+          <h1 className="text-center">You Have No Libraries :(</h1>
+          <div className="bg--light padding--med margin-btm--med text-center">
+            <h3>Creating a library is simple!</h3>
+            <div>
+              <p>Paste YouTube video URLs, then tag them with categories.</p>
+              <p>Patflix will generate the layout based on your tags, and create a shareable link.</p>
+            </div>
+            <Link className="btn btn-primary full-width" to={"/dashboard"}>Create A Library</Link>
+          </div>
+          {/*
+          <div className="bg--light padding--med margin-btm--med">
+            <p>Patflix lets you create organized libraries</p>
+            <p>Paste YouTube video URLs, add categories</p>
+            <Link className="btn btn-primary" to={"/dashboard/"}>Create A Library</Link>
+          </div>
+          */}
+        </div>
+      </div>
+      );
     }
     let libs = this.renderLibraries().map((library, index) => {
       return (<div key={index} className="bg--med">
