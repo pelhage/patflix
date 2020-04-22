e23d5b4e798d539286ef1acd96ce4a55a3a787a6
Patrick El-Hage <patrick.elhage@gmail.com>
2020-04-22 14:26:07 -0700

    Flatten js dir into src

--- client/config/paths.js
+++ client/config/paths.js
@@ -10,7 +10,7 @@ module.exports = {
   appHtml: resolveApp('index.html'),
   appSass: resolveApp('src/scss'),
   appPackageJson: resolveApp('package.json'),
-  appSrc: resolveApp('src/js'),
+  appSrc: resolveApp('src/'),
   appNodeModules: resolveApp('node_modules'),
   ownNodeModules: resolveApp('node_modules')
 }
--- client/src/components/Library/ViewLib.js
+++ client/src/components/Library/ViewLib.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../../actions'
-import history from '../../../routing/history'
+import history from '../../routing/history'
 
 import Library from './Library'
 
