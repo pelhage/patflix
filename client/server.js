b2e8cd64e020a4e0709e11acbd297fac5976370c
Patrick El-Hage <patrick.elhage@gmail.com>
2020-11-29 19:52:58 -0800

    remove pug + use sass-loader

--- client/server.js
+++ client/server.js
@@ -5,17 +5,21 @@ const webpack = require('webpack')
 const webpackConfig = require('./config/webpack.config.prod')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
-
-webpackConfig.entry.main.unshift('webpack-hot-middleware/client?reload=true&timeout=1000')
+const path = require('path')
+webpackConfig.entry.main.unshift(
+  'webpack-hot-middleware/client?reload=true&timeout=1000'
+)
 //Add HMR plugin
 webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin())
 const compiler = webpack(webpackConfig)
 
 //Enable "webpack-dev-middleware"
-app.use(webpackDevMiddleware(compiler, {
-  writeToDisk: true,
-  publicPath: webpackConfig.output.publicPath
-}))
+app.use(
+  webpackDevMiddleware(compiler, {
+    writeToDisk: true,
+    publicPath: webpackConfig.output.publicPath,
+  })
+)
 
 //Enable "webpack-hot-middleware"
 app.use(webpackHotMiddleware(compiler))
@@ -23,15 +27,12 @@ app.use(webpackHotMiddleware(compiler))
 // for parsing application/json
 app.use(bodyParser.json())
 
-
 // Set Up View Engine + Static Directory
-app.set('view engine', 'pug')
-app.use('/dist', express.static('dist'));
-app.use('/media', express.static('dist/media'));
+app.use('/dist', express.static('dist'))
+app.use('/media', express.static('dist/media'))
 
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
 
-app.get('*', function(req, res) {
-  res.render('index');
-});
-
-app.listen(3000);
+app.listen(3000)
